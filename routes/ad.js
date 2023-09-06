93b860ba737a5118819f835694352a447d9f9700
Nguyen Chi Thien <ncthien@gamejam.co>
2023-09-06 13:15:40 +0700

    Test returning html in JSON

--- routes/ad.js
+++ routes/ad.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+var fsp = require('fs/promises');
+
+const minify = require("html-minifier").minify;
+
 router.get('/', async function(req, res, next) {
   try {
+    const adData = await fsp.readFile('./data/banner.html');
+    const adString = adData.toString();
+    
+    const adStringFinal = minify(adString, {
+      collapseWhitespace: true,
+      removeComments: true,
+      minifyJS: true
+    });
+      
     let ret = {a: 1}
+    ret["html"] = adStringFinal;
+    
     res.json(ret);
   } catch (err) {
     console.error(`Error while processing ad request `, err.message);
