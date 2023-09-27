bb4368dc788a395424b7b965e2d042a02e30bad0
Nguyen Chi Thien <ncthien@gamejam.co>
2023-09-27 17:54:32 +0700

    Correct OMSDK verification message parse

--- routes/omidVerify.js
+++ routes/omidVerify.js
@@ -5,9 +5,23 @@ router.get('/', async function(req, res, next) {
   try {
     let msg = req.query.msg;
     
-    let data = JSON.parse(decodeURI(msg));
+    msg = decodeURI(msg);
     
-    console.log('Receive data: ' + JSON.stringify(data)); 
+    let separatorPos = msg.indexOf("::");
+    if (separatorPos < 0)
+    {
+      console.error(`Unable to parse message `, msg);
+    }
+    else
+    {
+      let dateString = msg.substring(0, separatorPos);
+      let date = new Date(dateString);
+      
+      console.log('Time: ' + date.toString()); 
+      
+      let data = JSON.parse(msg.substring(separatorPos + 2));
+      console.log('Data: ' + JSON.stringify(data)); 
+    }
     res.sendStatus(200);
   } catch (err) {
     console.error(`Error while processing OMID verify url `, err.message);
